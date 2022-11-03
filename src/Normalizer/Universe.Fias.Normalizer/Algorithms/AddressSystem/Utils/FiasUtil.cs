d967b57ec76c6480c250748439453dd93819c02e
Alex Envision <a.envision@hotmail.com>
2022-11-03 09:47:43 +0400

    [Universe.Fias.Normalizer] Сокращение пр-кт

--- src/Normalizer/Universe.Fias.Normalizer/Algorithms/AddressSystem/Utils/FiasUtil.cs
+++ src/Normalizer/Universe.Fias.Normalizer/Algorithms/AddressSystem/Utils/FiasUtil.cs
@@ -114,7 +114,8 @@ namespace Universe.Fias.Normalizer.Algorithms.AddressSystem.Utils
 
             var types = new MatList<AddressTypeDto>();
             types += new AddressTypeDto { Id = ++lastIndex, Name = "Проспект", ShortName = "п-кт", Level = 65 };
-			types += new AddressTypeDto { Id = ++lastIndex, Name = "Проспект", ShortName = "пр.", Level = 65 };
+            types += new AddressTypeDto { Id = ++lastIndex, Name = "Проспект", ShortName = "пр-кт", Level = 65 };
+            types += new AddressTypeDto { Id = ++lastIndex, Name = "Проспект", ShortName = "пр.", Level = 65 };
             types += new AddressTypeDto { Id = ++lastIndex, Name = "Проспект", ShortName = "просп", Level = 65 };
 
             types += new AddressTypeDto { Id = ++lastIndex, Name = "Проезд", ShortName = "пр.", Level = 65 };
